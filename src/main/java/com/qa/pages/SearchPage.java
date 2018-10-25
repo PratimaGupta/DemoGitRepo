39f1cb118c49e951efba2cacfe475c90fb8b1d0d
Pratima <pratimagupta82@yahoo.com>
2018-10-25 10:13:45 -0700

    Adding new files and updating exsting one

---
+++ src/main/java/com/qa/pages/SearchPage.java
@@ -0,0 +1,11 @@
+package com.qa.pages;
+
+public class SearchPage {
+	
+	public void testSearch() {
+		System.out.println("I am in testSearch again");
+		
+		System.out.println("I am in testSearch again123s");
+	}
+
+}
--- src/main/java/com/qa/pages/TestPages.java
+++ src/main/java/com/qa/pages/TestPages.java
@@ -4,6 +4,7 @@ public class TestPages {
 	
 	public void testPagesClass() {
 		System.out.println("I am in testPages ");
+		System.out.println("I am again testing");
 	}
 
 }
