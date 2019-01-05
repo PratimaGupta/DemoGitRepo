4b2af8a965935002b242591bc2b860ba546d2b2e
Pratima <pratimagupta82@yahoo.com>
2019-01-05 23:06:55 +0530

    latest c

--- src/main/java/com/qa/pages/TestPages.java
+++ src/main/java/com/qa/pages/TestPages.java
@@ -5,6 +5,7 @@ public class TestPages {
 	public void testPagesClass() {
 		System.out.println("I am in testPages ");
 		System.out.println("I am again testing");
+		System.out.println("I am testin....);
 	}
 
 }
